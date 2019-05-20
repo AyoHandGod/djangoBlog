a54201aa209e15eaddab94b92272cd8e0e7c73d5
Dayo <Calikidd86@gmail.com>
2019-05-20 15:57:50 -0700

    Updated post detail.html to display the Blog name as a link to homepage and the article title to no longer be a link.
    Added Pagination support to category and post list methods in
    blog/post.py. Added pagination.html and linked to post/list.html. Added
    logger to blog/views.py.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
+from django.utils.log import AdminEmailHandler
+import logging
+
 from .models import Post, Category
 
 
 # Create your views here.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('spam.log')
+emailHandler = AdminEmailHandler(include_html=True)
+logger.addHandler(fh)
+logger.addHandler(emailHandler)
+
 
 # Post Methods
 def list_post(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
+    paginator = Paginator(posts, 5)  # posts each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If the page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If the page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    context = {'posts': posts, 'page': page}
     return render(request, 'blog/post/list.html', context)
 
 
@@ -24,7 +47,15 @@ def post_detail(request, year, month, day, post):
 # category methods
 def list_categories(request):
     categories = Category.objects.all()
-    context = {'categories': categories}
+    paginator = Paginator(categories, 3)
+    page = request.GET.get('page')
+    try:
+        categories = paginator.page(page)
+    except PageNotAnInteger:
+        categories = paginator.page(1)
+    except EmptyPage:
+        categories = paginator.page(paginator.num_pages)
+    context = {'categories': categories, 'page': page}
     return render(request, 'blog/category/list.html',
                   context)
 
