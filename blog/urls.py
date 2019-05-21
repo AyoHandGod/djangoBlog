1748f790238ffa28485eb2e5d0594781b9ecff40
Dayo <Calikidd86@gmail.com>
2019-05-20 17:51:25 -0700

    Added PostDetailView class. Updated url to support this change. Fixed html issue on category/detail.html.

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     #url(r'^$', views.list_post, name='list_post'),
     path('', views.PostListView.as_view(), name='list_post'),
-    path('blog/<int:year>/<int:month>/<int:day>/<slug:post>/',
+    path('blog/<int:year>/<int:month>/<int:day>/<slug:slug>/',
          views.PostDetailView.as_view(), name='post_detail'),
     path('categories/', views.list_categories, name='list_category'),
     path('categories/<slug:category>/', views.category_details, name='category_details'),
