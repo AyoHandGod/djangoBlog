98176caa4313584dc01a4f7b307da7f425d5f866
Dayo <Calikidd86@gmail.com>
2019-06-01 19:12:56 -0700

    Added CommentModel to the Admin page.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 # Register your models here.
 @admin.register(Post)
@@ -20,3 +20,9 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
